ddde5d30afffc190cc337b446eb930c0c6c4d17c
Sardor Narzullaev <mr.narzullaev@gmail.com>
2018-03-30 20:55:32 +0800

    syntax errors are fixed

--- animal_kingdom/animalKingdom.pl
+++ animal_kingdom/animalKingdom.pl
@@ -1,53 +1,53 @@
 
-#creating list of facts
-#Animalia tree starts here
-#facts about Arthropoda kingdom
+/*creating list of facts*/
+/*Animalia tree starts here*/
+/*facts about Arthropoda kingdom*/
 in(housefly, domestica).
 in(domestica, musca).
 in(musca, musidae).
 in(musidae, diptera).
 in(diptera, insect).
 in(insect, arthropoda).
-#Arthropoda kingdom finishes here
+/*Arthropoda kingdom finishes here*/
 
-#Chordate Kingdom starts here
-#Carnivora tree starts here
+/*Chordate Kingdom starts here*/
+/*Carnivora tree starts here*/
 in(lion, leo).
 in(leo, felis).
 in(house_cat, domestica).
 in(domestica, felis).
 in(felis, felidae).
 in(felidae, carnivora).
-#carnivora tree finishes here
+/*carnivora tree finishes here*/
 
-#primate tree starts here
-#pongidae tree starts here
+/*primate tree starts here*/
+/*pongidae tree starts here*/
 in(chimpanzee, troglodytes).
 in(troglodytes, pan).
 in(pan, pongidae).
 in(pongidae, primate).
-#pongidae tree finishes here
+/*pongidae tree finishes here*/
 
-#hominidae tree starts here
+/*hominidae tree starts here*/
 in(human, spiens).
 in(spiens, homo).
 in(homo, hominidae).
-#hominidae tree finishes here
+/*hominidae tree finishes here*/
 
 in(hominidae, primate).
 in(pongidae, primate).
-#primate tree finishes here
+/*primate tree finishes here*/
 
 in(carnivora, mammal).
 in(primate, mammal).
-#mammal tree finishes here
+/*mammal tree finishes here*/
 
 in(mammal, chordate)
-#Chordate tree finishes here
+/*Chordate tree finishes here*/
 
 in(chordate, animalia).
 in(arthropoda, animalia).
-#animal Kingdom tree finishes here
+/*animal Kingdom tree finishes here*/
 
 go:-
 nl,
@@ -75,13 +75,12 @@ nl, nl,
 restart.
 
 
-#is_in(X,Y):-
-#write('Invalid animal detected!'),
-#nl.
+is_in(X,Y):-
+write('Invalid animal detected!'),
+nl.
 
 restart :-
-	nl,nl
-,	write('Would you like to continue? (yes or  no)'),
+	nl,nl, write('Would you like to continue? (yes or  no)'),
 	read(Y),
 	continue(Y).
 continue(no):-
